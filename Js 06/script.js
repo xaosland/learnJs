0c7e669a0f331b43de0dd3f48db24f725621b542
xaosland <xaosland@mail.ru>
2024-06-13 18:47:37 +0400

    learn js 06

---
+++ Js 06/script.js
@@ -0,0 +1,15 @@
+const str = "Hello";
+
+console.log(str.length)
+
+const arr = [1, 2, 3, 4, 5];
+
+console.log(arr.length)
+
+console.log(str.toUpperCase())
+console.log(str)
+
+const lao = "22.2px"
+console.log(str.slice(0, 3))
+
+console.log(parseInt(lao))
